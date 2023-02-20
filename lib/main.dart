24b81c8bbafe9936c73172f5539783611bf33938
haroon <m.haroon.mazhar@gmail.com>
2023-02-20 12:39:06 +0500

    uuuY

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+
+
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  final String temp = "hello";
+  String tem = "hey";
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(temp),
+          ElevatedButton(onPressed: (){
+            setState(() {
+              tem = "world is my";
+            });
+          }, child: Text("click my name"))
+
+        ],
+      ),
+    );
+  }
+}
+
